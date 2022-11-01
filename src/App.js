05f7d95ea2b36e1982d5cdce36c15dfdec675be4
Herman Omonya <112311947+omonyaherman@users.noreply.github.com>
2022-11-02 02:17:52 +0300

    standars

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React,{ useState} from 'react';
 import './App.css';
 import Card from './components/Card'
 
 function App() {
-
-
   return (
     <div className="App">
      <Card name={'Jenifer Kessy'} job={'Front-end Software Developer'}/>
